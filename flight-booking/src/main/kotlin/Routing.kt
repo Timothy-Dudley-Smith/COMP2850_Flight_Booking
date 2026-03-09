b4d87f5a484d0af1e09ea218938068550e7d3e78
Timothy-Dudley-Smith <dudleysmithtim@gmail.com>
2026-03-09 16:43:15 +0000

    Add homepage

--- flight-booking/src/main/kotlin/Routing.kt
+++ flight-booking/src/main/kotlin/Routing.kt
@@ -3,11 +3,11 @@ package com.example.com
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import io.ktor.http.*
+import io.ktor.server.http.content.*
 
 fun Application.configureRouting() {
     routing {
-        get("/") {
-            call.respondText("Hello World!")
-        }
+        staticResources("/", "static")
     }
 }
