a05404a415cf567c8f568d29c91430205cce5770
Musaddak14 <mnvr3540@leeds.ac.uk>
2026-03-13 03:12:11 +0000

    getAvailableSeats added

--- flight-booking/src/main/kotlin/com/flightsystem/service/BookingService.kt
+++ flight-booking/src/main/kotlin/com/flightsystem/service/BookingService.kt
@@ -3,11 +3,14 @@ package com.flightsystem.service
 import com.flightsystem.model.*
 
 class BookingService {
+    // booking storage list
+    private val bookings = mutableListOf<Booking>()
 
     fun calculateTotalPrice(flight: Flight, seats: List<Seat>): Double {
         return flight.price * seats.size
     }
 
+    // check if seat is available
     fun validateSeats(requestedSeats: List<Seat>): Boolean {
         for (seat in requestedSeats) {
             if(!seat.isAvailable) {
@@ -17,6 +20,7 @@ class BookingService {
         return true    
     }
 
+    // create booking, use validateSeats()
     fun createBooking(
         bookingID: String,
         user: User,
@@ -34,18 +38,32 @@ class BookingService {
         }
 
 
-        return Booking(
+        val Booking(
             bookingId = bookingID,
             user = user, 
             flight = flight, 
             seatsBooked = requestedSeats, 
             payment = payment
         )
+        bookings.add(booking)
+        return booking 
     }
 
     fun cancelBooking(booking: Booking) {
         for(seat in booking.seatsBooked) {
             seat.isAvailable = true
         }
+    }
+
+    // return a list of all seats that are still available 
+    fun getAvailableSeats(flight: Flight): List<Seat> {
+        val availableSeats = mutableListOf<Seat>()
+
+        for(seat in flight.seats) {
+            if (seat.isAvailable) {
+                availableSeats.add(seat)
+            }
+        }
+        return availableSeats
     }
 }
