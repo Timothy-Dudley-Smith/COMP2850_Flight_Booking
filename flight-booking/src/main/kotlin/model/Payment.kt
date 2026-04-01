313da384a04231ba0ab4fdf2b52ac411fb434194
mikaeel4760-arch <mikaeelm06@gmail.com>
2026-04-01 19:22:05 +0100

    Started Payment Logic

--- flight-booking/src/main/kotlin/model/Payment.kt
+++ flight-booking/src/main/kotlin/model/Payment.kt
@@ -1,14 +1,44 @@
 package com.flightsystem.model
 
+import java.time.LocalDateTime
+
 import com.flightsystem.model.Bookings.bookingId
 import org.jetbrains.exposed.sql.Table
 
+
+enum class PaymentStatus {
+    PENDING,
+    SUCCESS,
+    FAILED,
+    REFUNDED
+}
+
+
 data class Payment(
-    val paymentId: Int,
-    val bookingId: Int,
+    val paymentID: String,
+    val bookingID: String,
+    val userID: String,
     val amount: Double,
-    val status: Boolean
-)
+    val lastfourdigits: String, number
+    val cardHolderName: String,
+    val status: PaymentStatus = PaymentStatus.PENDING,
+    val timestamp: LocalDateTime = LocalDateTime.now(),
+    val refundedAt: LocalDateTime? = null
+) {
+
+
+
+    fun setstatusSuccess() {
+        status = PaymentStatus.SUCCESS
+    }
+
+    fun setStatusFailed() {
+        status = PaymentStatus.FAILED
+    }
+
+
+
+}
 
 object Payments : Table() {
     val paymentId = integer("paymentId").autoIncrement()
